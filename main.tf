ea5ab5507f76ea7260a401eda32ac579c3f10542
Matt Willsher <matt@monki.org.uk>
2021-07-20 21:28:26 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.50"
+    }
+  }
+
+  required_version = "~> 1.0.2"
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "content" {
+  bucket = "willsher.systems"
+  acl    = "public-read"
+}
